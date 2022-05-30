f1c51c2473f10d651842cebf816d171a84aba38f
matimarco <chunoss@gmail.com>
2022-05-30 15:31:30 -0300

    Initial commit, Adding base project and dependencies

---
+++ src/test/java/com/challenge/mendel/MendelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.mendel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MendelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
