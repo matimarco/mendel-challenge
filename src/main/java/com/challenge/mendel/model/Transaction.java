58f44c2c3120b8133bd01c80cca72a9d1f265fb0
matimarco <matimarco@hotmail.com>
2022-05-30 15:35:37 -0300

    Adding model

---
+++ src/main/java/com/challenge/mendel/model/Transaction.java
@@ -0,0 +1,42 @@
+package com.challenge.mendel.model;
+
+public class Transaction {
+
+    private long transactionId;
+    private double amount;
+    private String type;
+    private long parentId;
+
+
+    public long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+}
